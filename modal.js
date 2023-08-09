6f9593d632a3f612a38a8096a20a906c3718cc7b
NikiforovDima8 <dimasya88nikiforov@gmal.com>
2023-08-09 17:38:03 +0300

    modal

---
+++ modal.js
@@ -0,0 +1,30 @@
+// const popupLinks=document.querySelectorAll('.popup-link')
+// const body=document.querySelector('body') // для того чтобы убрать скролл
+// const lockPadding=document.querySelectorAll('.lock-padding')
+
+// let unlock=true // чтобы не было двойных нажатий 
+
+// const timeOut=800
+
+// const nav = document.querySelector('#nav');
+// const navBtn = document.querySelector('#nav-btn');
+// const navBtnImg = document.querySelector('#nav-btn-img');
+
+// navBtn.addEventListener('click',()=>{
+//     if (nav.classList.toggle('open')) {
+//      navBtnImg.src ='./img/close.svg';
+//     } else{
+//         navBtnImg.src ='./img/NAV.svg';
+//     }
+// })
+
+// if(nav.classList.toggle('open')){
+//     nav.style.display=='flex'
+// } else{
+//     nav.style.display=='none'
+// }
+    
+
+const popupLinksBtn=document.querySelector('#popup-link')  // кнопка открывания
+const popupOpen=document.querySelector('#popup') // открытие модального окна
+const popupClose=document.querySelector('#close-popup')
